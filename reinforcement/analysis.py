4f93c73dec20ae4d1cf6d997b555ac7dcf5c3064
Alan <akbouwma@mtu.edu>
2020-12-03 08:42:27 -0500

    q8 done

--- reinforcement/analysis.py
+++ reinforcement/analysis.py
@@ -60,9 +60,9 @@ def question3e():
     # If not possible, return 'NOT POSSIBLE'
 
 def question8():
-    answerEpsilon = None
-    answerLearningRate = None
-    return answerEpsilon, answerLearningRate
+    answerEpsilon = 0.05
+    answerLearningRate = 0.001
+    return 'NOT POSSIBLE'
     # If not possible, return 'NOT POSSIBLE'
 
 if __name__ == '__main__':
